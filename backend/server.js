88a240f4a4b6ab69b130e770fcdcc750c02ed216
Francisco Vazquez <francisco.v@brillio.com>
2022-11-30 12:25:07 -0600

    - express server Implementation, - errorHandler middleware function - express-async-handler package installation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 8000;
+const app = express();
+const { errorHandler } = require("./middleware/errorMiddleware");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+// Routes
+app.get("/", (req, res) =>
+  res.status(201).json({ message: "Hello from JSON Response" })
+);
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Error Handler Middleware must be implemented after all Routes
+app.use(errorHandler);
