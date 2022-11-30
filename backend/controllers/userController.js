74f21a975f56c78e5bef1b6462a48e9702c69a67
Francisco Vazquez <francisco.v@brillio.com>
2022-11-30 16:58:48 -0600

    Private Routes using JWT validation Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 // @desc Login a user
-// @route /api/login
+// @route /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -62,7 +62,21 @@ const generateToken = (id) => {
   });
 };
 
+// @desc Get Current User
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.email,
+  };
+
+  res.status(200).json(user);
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
+const authJWT = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authJWT, getMe);
 
 module.exports = router;
