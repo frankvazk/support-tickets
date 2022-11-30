48b7c965ef11b4e5105b7e7294454e08f7c1af80
Francisco Vazquez <francisco.v@brillio.com>
2022-11-30 13:20:25 -0600

    MogoDB Connection File

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `Mongo DB Connected ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.log(`Error: ${error}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 8000;
-const app = express();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+//Connect to Database
+connectDB();
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
