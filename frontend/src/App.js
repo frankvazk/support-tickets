45ae2f94c12bbad0602144b0c3dd328a876b9569
Francisco Vazquez <francisco.v@brillio.com>
2022-11-30 18:12:35 -0600

    App Boilerplate (Home, Register, Login, Header)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
