c0a28160c2dcfae3d790a70a8296df1903c78175
Francisco Vazquez <francisco.v@brillio.com>
2022-12-07 17:54:34 -0600

    MongoParseError: options usecreateindex, usefindandmodify are not supported --fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,7 @@ const asyncHandler = require("express-async-handler");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(
       `Mongo DB Connected ${conn.connection.host}`.cyan.underline.bold
     );
